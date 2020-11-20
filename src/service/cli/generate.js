976ec221626dab387eecb79403bb6243ed443ade
Кирилл Черепанов <ktcherepanov@nologostudio.ru>
2020-11-20 19:27:24 +0300

    Completed data files with data for mocks

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -10,55 +10,9 @@ const {
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `../../mock.json`;
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`
-];
-
-const SENTENCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`
-];
+const FILE_CATEGORIES_PATH = `../../data/categories.txt`;
+const FILE_SENTENCES_PATH = `../../data/sentences.txt`;
+const FILE_TITLES_PATH = `../../data/titles.txt`;
 
 const DateRestrict = {
   MIN: 0,
@@ -71,24 +25,37 @@ const generateDate = () => {
   return new Date(time).toUTCString();
 };
 
-const generatePosts = (count) => (
+const generatePosts = (count, titles, sentences, categories) => (
   Array(count)
     .fill({})
     .map(() => ({
-      title: TITLES[getRandomInt(0, TITLES.length - 1)],
+      title: titles[getRandomInt(0, titles.length - 1)],
       createdDate: generateDate(),
-      announce: shuffle(SENTENCES).slice(1, 5).join(` `),
-      fullText: shuffle(SENTENCES).slice(1, 10).join(` `),
-      category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
+      announce: shuffle(sentences).slice(1, 5).join(` `),
+      fullText: shuffle(sentences).slice(1, 10).join(` `),
+      category: [categories[getRandomInt(0, categories.length - 1)]],
     }))
 );
+
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf-8`);
+    return content.split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
 
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const titles = await readContent(FILE_TITLES_PATH);
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
     const [count] = args;
     const countPosts = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generatePosts(countPosts));
+    const content = JSON.stringify(generatePosts(countPosts, titles, sentences, categories));
 
     try {
       await fs.writeFile(FILE_NAME, content);
