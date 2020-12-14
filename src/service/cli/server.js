ae5cea4634cdc48baa23b37436087fdcdf5e5ead
Кирилл Черепанов <ktcherepanov@nologostudio.ru>
2020-12-14 11:43:03 +0300

    rewrote cli services server on express and deleted excess templates

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -2,67 +2,44 @@
 
 const chalk = require(`chalk`);
 const fs = require(`fs`).promises;
-const http = require(`http`);
+const express = require(`express`);
+const { HttpCode } = require(`../../constants`);
 
 const DEFAULT_PORT = 3000;
-const httpCode = {
-  OK: 200,
-  NOT_FOUND: 404,
-};
 const FILE_NAME = `mock.json`;
 
-const sendResponse = (res, options) => {
-  const [statusCode, message] = options;
-  const template = `
-    <!Doctype html>
-    <html lang="ru">
-      <head>
-        <title>From Node with love!</title>
-      </head>
-      <body>${message}</body>
-    </html>`;
+const app = express();
 
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-
-  res.end(template)
-};
-
-const onServerResponse = async (req, res) => {
-  const notFoundMessageTxt = `Resourse is not found!`;
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const fileContent = await fs.readFile(FILE_NAME);
-        const mocks = JSON.parse(fileContent);
-        const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, [httpCode.OK, `<ul>${message}</ul>`]);
-      } catch (err) {
-        sendResponse(res, [httpCode.NOT_FOUND, notFoundMessageTxt]);
-      }
-      break;
-      default:
-        sendResponse(res, [httpCode.NOT_FOUND, notFoundMessageTxt]);
-        break;
+app.use(express.json());
+app.get(`/posts`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(FILE_NAME);
+    const mocks = JSON.parse(fileContent);
+    res.json(mocks);
+  } catch (err) {
+    res
+      .status(HttpCode.INTERNAL_SERVER_ERROR)
+      .send(err);
   }
-};
+});
+
+app.use((req, res) => {
+  res
+    .status(HttpCode.BAD_REQUEST)
+    .send(`Not found`);
+});
 
 module.exports = {
   name: `--server`,
   run(args) {
     const [userPort] = args;
     const serverPort = Number.parseInt(userPort, 10) || DEFAULT_PORT;
-    http.createServer(onServerResponse)
-      .listen(serverPort)
-      .on(`listening`, (err) => {
-        if (err) {
-          return console.error(chalk.red(`Error while creating server: ${err}`));
-        }
+    app.listen(serverPort, (err) => {
+      if (err) {
+        return console.error(chalk.red(`Error while creating server: ${err}`));
+      }
 
-        return console.info(chalk.green(`Server start on port ${serverPort}`));
-      })
+      return console.info(chalk.green(`Server start on port ${serverPort}`));
+    });
   }
 };
