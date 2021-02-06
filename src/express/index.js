13520d5b83461b836e0ba1e6d6171db731eabe12
Кирилл Черепанов <ktcherepanov@nologostudio.ru>
2021-02-06 22:46:49 +0300

    created api for get-send data between front-server and api

--- src/express/index.js
+++ src/express/index.js
@@ -13,11 +13,13 @@ const {HttpCode} = require(`../constants`);
 const app = express();
 const PORT = 8000;
 const PUBLIC_DIR = `public`;
+const UPLOAD_DIR = `upload`;
 
 app.set(`views`, path.resolve(__dirname, `./templates`));
 app.set(`view engine`, `pug`);
 
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.use(express.static(path.resolve(__dirname, UPLOAD_DIR)));
 
 app.use(`/`, mainRoutes);
 app.use(`/articles`, articlesRoutes);
--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -2,9 +2,12 @@
 
 const {Router} = require(`express`);
 const mainRoutes = new Router();
+const api = require(`../api`).getAPI();
 
-mainRoutes.get(`/`, (req, res) => {
-  res.render(`main`);
+mainRoutes.get(`/`, async (req, res) => {
+  const body = await api.getArticles();
+  const articles = body.data;
+  res.render(`main`, {articles});
 });
 
 mainRoutes.get(`/register`, (req, res) => {
@@ -16,7 +19,20 @@ mainRoutes.get(`/login`, (req, res) => {
 });
 
 mainRoutes.get(`/search`, (req, res) => {
-  res.render(`search`, {isColorBackground: `baseColor`});
+  res.render(`search`, {
+    isColorBackground: `baseColor`
+  });
+});
+
+mainRoutes.get(`/search-result`, async (req, res) => {
+  const {search} = req.query;
+  const results = await api.search(search);
+
+  res.render(`search-result`, {
+    isColorBackground: `baseColor`,
+    search,
+    results
+  });
 });
 
 mainRoutes.get(`/categories`, (req, res) => {
